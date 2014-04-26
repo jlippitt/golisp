5f9fff2bc9df90787528b58df0b20648a9ba3da9
James Lippitt <james.lippitt@gmail.com>
2014-04-26 16:27:35 +0100

    Similar clean-up for VM

--- vm.go
+++ vm.go
@@ -28,31 +28,59 @@ const (
 func run(code byteStream) cell {
 	var op byte
 	var err error
+	var opNil, opLdc, opAdd func()
 
-	theVm := vm{
-		S: newNilCell(),
-		E: newNilCell(),
-		C: code,
-		D: newNilCell(),
+	var stack cell = newNilCell()
+	//var env cell = newNilCell()
+	//var dump cell = newNilCell()
+
+	opNil = func() {
+		push(&stack, newNilCell())
+		log.Print("NIL")
 	}
 
-	jumpTable := []func(*vm){
+	opLdc = func() {
+		var bytes [8]byte
+		var intVal int64 = 0
+
+		if _, err := code.Read(bytes[:]); err != nil {
+			panic(err)
+		}
+
+		for _, byte := range bytes {
+			intVal = intVal << 8
+			intVal += int64(byte)
+		}
+
+		push(&stack, newFixNumCell(intVal))
+
+		log.Printf("LDC %d", intVal)
+	}
+
+	opAdd = func() {
+		rhs := pop(&stack).(*fixNumCell).Value()
+		lhs := pop(&stack).(*fixNumCell).Value()
+		push(&stack, newFixNumCell(lhs+rhs))
+		log.Printf("ADD")
+	}
+
+	jumpTable := []func(){
 		opNil,
 		opLdc,
 		opAdd,
 	}
 
 	for {
-		op, err = theVm.C.ReadByte()
+		op, err = code.ReadByte()
 
 		if err != nil {
 			break
 		}
 
-		jumpTable[op](&theVm)
+		jumpTable[op]()
 	}
 
-	switch returnValue := theVm.S.(type) {
+	switch returnValue := stack.(type) {
 	case *consCell:
 		return returnValue.Car()
 	case *nilCell:
@@ -60,37 +88,4 @@ func run(code byteStream) cell {
 	default:
 		panic("Unexpected cell type on stack")
 	}
-}
-
-func opNil(vm *vm) {
-	push(&vm.S, newNilCell())
-
-	log.Print("NIL")
-}
-
-func opLdc(vm *vm) {
-	var bytes [8]byte
-	var intVal int64 = 0
-
-	if _, err := vm.C.Read(bytes[:]); err != nil {
-		panic(err)
-	}
-
-	for _, byte := range bytes {
-		intVal = intVal << 8
-		intVal += int64(byte)
-	}
-
-	push(&vm.S, newFixNumCell(intVal))
-
-	log.Printf("LDC %d", intVal)
-}
-
-func opAdd(vm *vm) {
-	rhs := pop(&vm.S).(*fixNumCell).Value()
-	lhs := pop(&vm.S).(*fixNumCell).Value()
-
-	push(&vm.S, newFixNumCell(lhs+rhs))
-
-	log.Printf("ADD")
 }
