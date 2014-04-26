4a241653daa643c485c64739fc91eaff37dab580
James Lippitt <james.lippitt@gmail.com>
2014-04-26 16:22:11 +0100

    Clean up parser implementation slightly

--- parser.go
+++ parser.go
@@ -1,50 +1,54 @@
 package main
 
 func parse(input string) cell {
-	return parseExpression(newTokenizer(input))
-}
+	var parseExpression, parseCons func() cell
 
-func parseExpression(tokenizer *tokenizer) cell {
-	var value cell
+	tokenizer := newTokenizer(input)
 
-	switch tokenizer.Type() {
-	case TOK_OPEN:
-		value = parseCons(tokenizer)
-	case TOK_SYMBOL:
-		value = newSymbolCell(tokenizer.StringValue())
-	case TOK_FIXNUM:
-		value = newFixNumCell(tokenizer.IntValue())
-	default:
-		panic("Unexpected token")
-	}
+	parseExpression = func() cell {
+		var value cell
 
-	return value
-}
-
-func parseCons(tokenizer *tokenizer) cell {
-	var cons cell = newNilCell()
-	var current *cell = &cons
-
-	for {
-		tokenizer.Next()
-
-		if tokenizer.Type() != TOK_CLOSE {
-			var newValue cell = newConsCell(parseExpression(tokenizer), newNilCell())
-
-			switch cons := (*current).(type) {
-			case *consCell:
-				cons.SetCdr(newValue)
-				current = &newValue
-			case *nilCell:
-				*current = newValue
-			default:
-				panic("Unexpected cell type in cons expression")
-			}
-
-		} else {
-			break
+		switch tokenizer.Type() {
+		case TOK_OPEN:
+			value = parseCons()
+		case TOK_SYMBOL:
+			value = newSymbolCell(tokenizer.StringValue())
+		case TOK_FIXNUM:
+			value = newFixNumCell(tokenizer.IntValue())
+		default:
+			panic("Unexpected token")
 		}
+
+		return value
 	}
 
-	return cons
+	parseCons = func() cell {
+		var cons cell = newNilCell()
+		var current *cell = &cons
+
+		for {
+			tokenizer.Next()
+
+			if tokenizer.Type() != TOK_CLOSE {
+				var newValue cell = newConsCell(parseExpression(), newNilCell())
+
+				switch cons := (*current).(type) {
+				case *consCell:
+					cons.SetCdr(newValue)
+					current = &newValue
+				case *nilCell:
+					*current = newValue
+				default:
+					panic("Unexpected cell type in cons expression")
+				}
+
+			} else {
+				break
+			}
+		}
+
+		return cons
+	}
+
+	return parseExpression()
 }
