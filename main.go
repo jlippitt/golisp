0f7f16668441b90fe050168e7637967c6320b9bc
James Lippitt <james.lippitt@gmail.com>
2014-05-04 11:03:31 +0100

    Read code from source files

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	var result cell
+	var code []byte
 	var err error
 
 	if len(os.Args) < 2 {
-		fmt.Println("No code to execute")
+		fmt.Println("No source file specified")
 		os.Exit(1)
 	}
 
-	result, err = Execute(os.Args[1])
+	if code, err = ioutil.ReadFile(os.Args[1]); err != nil {
+		fmt.Println("Could not read source file")
+		os.Exit(1)
+	}
+
+	result, err = Execute(string(code))
 
 	if err != nil {
 		fmt.Print("Error: " + err.Error())
